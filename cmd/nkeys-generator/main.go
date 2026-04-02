ed99fbec5ad8f1e92d9c2d71004033ec846db07d
n4djib <n4djib@gmail.com>
2026-04-02 14:11:42 +0100

    first commit

---
+++ cmd/nkeys-generator/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/nats-io/nkeys"
+)
+
+func main() {
+	kp, err := nkeys.CreateUser()
+	if err != nil {
+		panic(err)
+	}
+
+	pub, _ := kp.PublicKey()
+	seed, _ := kp.Seed()
+
+	// Save private key locally
+	err = os.WriteFile("leaf.nkey", seed, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Public key (send to server):", pub)
+}
