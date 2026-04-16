3ff5d84cb008f83f210a6a757a8fefc3bffe1f9b
n4djib <n4djib@gmail.com>
2026-04-16 20:59:51 +0100

    fix frontend hot-reload in dev + changes to vars

--- web/apps/remote/src/routes/index.tsx
+++ web/apps/remote/src/routes/index.tsx
@@ -1,7 +1,8 @@
+import { env } from "#/env";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-  return <div>Hello world from remote</div>;
+  return <div>Hello world from remote port:{env.VITE_FRONT_PORT}</div>;
 }
---
+++ web/apps/remote/src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+// interface ImportMetaEnv {
+//   readonly VITE_APP_NAME: string;
+//   readonly VITE_FRONT_PORT: string;
+// }
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
